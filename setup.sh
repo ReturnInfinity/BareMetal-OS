8dab79b68e8a53d1a07c2feed31c28f2b1a4a709
Ian Seyler <ian.seyler@returninfinity.com>
2013-06-20 15:33:15 -0400

    Adjust setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ git clone https://github.com/ReturnInfinity/BMFS.git
 git clone https://github.com/ReturnInfinity/Pure64.git
 git clone https://github.com/ReturnInfinity/BareMetal-OS.git
 dd if=/dev/zero of=build/bmfs.image bs=1M count=128
+cd build
+./build.sh
+./setup.sh
+./install.sh
