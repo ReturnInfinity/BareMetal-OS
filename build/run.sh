62096567b32e814aa57b291f1e3707a227e7966c
Ian Seyler <ian.seyler@returninfinity.com>
2013-07-02 11:47:31 -0400

    Disable network in QEMU

--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-x86_64 -smp 8 -m 256 -drive id=disk,file=bmfs.image,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS"
+qemu-system-x86_64 -smp 8 -m 256 -drive id=disk,file=bmfs.image,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS" -net nic,model=i82551
