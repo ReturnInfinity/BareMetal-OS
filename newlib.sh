68a7dccf736acdcc7ac14867495881698744933f
Ian Seyler <ian.seyler@returninfinity.com>
2014-01-16 16:27:41 -0500

    Newlib script additions.

--- newlib.sh
+++ newlib.sh
@@ -7,5 +7,32 @@ cd newlib
 wget ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz
 tar xf newlib-2.1.0.tar.gz
 mkdir build
-cd ..
+
+cd ../src/BareMetal-OS/newlib/patches
+cp config.sub.patch ../../../../newlib/newlib-2.1.0/
+cp configure.host.patch ../../../../newlib/newlib-2.1.0/newlib/
+cp configure.in.patch ../../../../newlib/newlib-2.1.0/newlib/libc/sys/
+cd ../../../../newlib
+
+mkdir newlib-2.1.0/newlib/libc/sys/baremetal
+cp ../src/BareMetal-OS/newlib/baremetal/* newlib-2.1.0/newlib/libc/sys/baremetal/
+
+cd newlib-2.1.0/newlib/libc/sys
+autoconf
+cd baremetal
+autoreconf
+cd ../../../../../build
+
+../newlib-2.1.0/configure --target=x86_64-pc-baremetal --disable-multilib
+
+sed -i 's/TARGET=x86_64-pc-baremetal-/TARGET=/g' Makefile
+sed -i 's/WRAPPER) x86_64-pc-baremetal-/WRAPPER) /g' Makefile
+
+make
+
+cd x86_64-pc-baremetal/newlib/
+cp libc.a ../../..
+cp libm.a ../../..
+cp crt0.o ../../..
+cd ../../..
 
