a86c22770418c0b6984aeef7b5063ae6367e9f4c
Ian Seyler <ian.seyler@returninfinity.com>
2014-02-03 18:34:12 -0500

    Fix VDI script paths

--- vdi.sh
+++ vdi.sh
@@ -2,5 +2,5 @@
 
 cd bin
 qemu-img convert -O vdi bmfs.image BareMetal_OS.vdi
-dd if=VDI_UUID.bin of=bin/BareMetal_OS.vdi count=1 bs=512 conv=notrunc
+dd if=../VDI_UUID.bin of=BareMetal_OS.vdi count=1 bs=512 conv=notrunc
 
