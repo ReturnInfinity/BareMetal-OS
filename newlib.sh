8d638eea5fb71841bf9a5d54da8ec8302be7e1d4
Ian Seyler <ian.seyler@returninfinity.com>
2015-01-23 17:14:17 -0500

    Update for newlib v2.2.0

--- newlib.sh
+++ newlib.sh
@@ -7,18 +7,18 @@ cd newlib
 
 echo Downloading Newlib
 
-wget ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz
-tar xf newlib-2.1.0.tar.gz
+wget ftp://sourceware.org/pub/newlib/newlib-2.2.0.tar.gz
+tar xf newlib-2.2.0.tar.gz
 mkdir build
 
 echo Configuring Newlib
 
 cd ../src/BareMetal-OS/newlib/patches
-cp config.sub.patch ../../../../newlib/newlib-2.1.0/
-cp configure.host.patch ../../../../newlib/newlib-2.1.0/newlib/
-cp configure.in.patch ../../../../newlib/newlib-2.1.0/newlib/libc/sys/
+cp config.sub.patch ../../../../newlib/newlib-2.2.0/
+cp configure.host.patch ../../../../newlib/newlib-2.2.0/newlib/
+cp configure.in.patch ../../../../newlib/newlib-2.2.0/newlib/libc/sys/
 cd ../../../../newlib
-cd newlib-2.1.0
+cd newlib-2.2.0
 patch < config.sub.patch
 cd newlib
 patch < configure.host.patch
@@ -26,16 +26,16 @@ cd libc/sys
 patch < configure.in.patch
 cd ../../../..
 
-mkdir newlib-2.1.0/newlib/libc/sys/baremetal
-cp ../src/BareMetal-OS/newlib/baremetal/* newlib-2.1.0/newlib/libc/sys/baremetal/
+mkdir newlib-2.2.0/newlib/libc/sys/baremetal
+cp ../src/BareMetal-OS/newlib/baremetal/* newlib-2.2.0/newlib/libc/sys/baremetal/
 
-cd newlib-2.1.0/newlib/libc/sys
+cd newlib-2.2.0/newlib/libc/sys
 autoconf
 cd baremetal
 autoreconf
 cd ../../../../../build
 
-../newlib-2.1.0/configure --target=x86_64-pc-baremetal --disable-multilib
+../newlib-2.2.0/configure --target=x86_64-pc-baremetal --disable-multilib
 
 sed -i 's/TARGET=x86_64-pc-baremetal-/TARGET=/g' Makefile
 sed -i 's/WRAPPER) x86_64-pc-baremetal-/WRAPPER) /g' Makefile
@@ -56,7 +56,7 @@ echo Compiling test application...
 
 cp ../src/BareMetal-OS/newlib/*.* .
 
-gcc -I newlib-2.1.0/newlib/libc/include/ -c test.c -o test.o
+gcc -I newlib-2.2.0/newlib/libc/include/ -c test.c -o test.o
 ld -T app.ld -o test.app crt0.o test.o libc.a
 
 cp ../src/BareMetal-OS/programs/libBareMetal.* .
