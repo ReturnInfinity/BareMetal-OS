68a6971d3db775c61ae653041668107b8dc85afb
Mason X <ohnx@users.noreply.github.com>
2015-03-15 18:44:32 -0400

    Fix appc.sh
    Toying around with appc.sh

--- appc.sh
+++ appc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd src/BareMetal-OS/programs/
-gcc -I newlib-2.0.0/newlib/libc/include/ -c $1.c -o $1.o -DBAREMETAL
+gcc -I newlib/libc/include -c $1.c -o $1.o -DBAREMETAL
 ld -T app.ld -o $1.app crt0.o $1.o libc.a libBareMetal.o
 mv $1.app ../../../bin/
 if [ $? -eq 0 ]; then
