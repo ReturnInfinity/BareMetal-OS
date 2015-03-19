95f8ab87d1075c327eb1b445b52f5f9f61fc59f6
Mason X <ohnx@users.noreply.github.com>
2015-03-18 21:23:49 -0400

    Fix silly mistake
    How silly of me to forget a library.

--- appc.sh
+++ appc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd src/BareMetal-OS/programs/
 gcc -I newlib/libc/include -c $1.c -o $1.o -DBAREMETAL
-ld -T app.ld -o $1.app crt0.o $1.o libc.a libBareMetal.o
+ld -T app.ld -o $1.app crt0.o $1.o libc.a libm.a libBareMetal.o
 mv $1.app ../../../bin/
 if [ $? -eq 0 ]; then
 cd ../../../bin
