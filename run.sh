f864aa7b169f90776722a79112fe838f686147c9
Ian Seyler <ian@seyler.me>
2016-11-20 19:35:59 -0500

    Update run.sh
    Calls for an e1000 NIC now.
    Defaults to running in the command line.
    Remove “-curses” for standard QEMU behaviour.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd bin
-qemu-system-x86_64 -vga std -smp 8 -m 256 -drive id=disk,file=bmfs.image,if=none,format=raw -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS" -net nic,model=i82551
+qemu-system-x86_64 -smp 4 -m 256 -drive id=disk,file=bmfs.image,if=none,format=raw -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -name "BareMetal OS" -net nic,model=e1000 -curses
