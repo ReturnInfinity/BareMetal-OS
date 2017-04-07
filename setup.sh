fe9921d4a7a9b29ab0c6b7c7b81054efc7dcf49e
Ian Seyler <ian.seyler@returninfinity.com>
2017-04-07 12:11:21 -0400

    Use the bmfs utility to create the disk image

--- setup.sh
+++ setup.sh
@@ -3,29 +3,22 @@
 if [ ! -d "$src" ]; then
   mkdir src
 fi
+if [ ! -d "$bin" ]; then
+  mkdir bin
+fi
+
 cd src
 git clone https://github.com/ReturnInfinity/BMFS.git
 git clone https://github.com/ReturnInfinity/Pure64.git
 git clone https://github.com/ReturnInfinity/BareMetal-OS.git
 cd ..
 
-if [ ! -d "$bin" ]; then
-  mkdir bin
-fi
-platform=`uname`
-case "${platform}" in
-  Darwin)
-    dd if=/dev/zero of=bin/bmfs.image bs=1m count=128
-    ;;
-  *)
-    dd if=/dev/zero of=bin/bmfs.image bs=1M count=128
-    ;;
-esac
-
 cd src/BMFS
 make
 mv bmfs ../../bin/
-cd ../..
+cd ../../bin
+./bmfs bmfs.image initialize 128M
+cd ..
 
 ./build.sh
 ./format.sh
