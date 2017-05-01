a992be055182093e406f2d0275b279c50651aa07
Taylor Holberton <taylorcholberton@gmail.com>
2017-05-01 14:59:59 -0700

    building and installing alloy.app

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cd bin
 echo Writing Pure64+Software
 cat pure64.sys kernel64.sys > software.sys
 dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc
+./bmfs bmfs.image create alloy.app 2M
+./bmfs bmfs.image write alloy.app
