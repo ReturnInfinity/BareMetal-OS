3ec330820066f67a53947cb3a6f1e5992d1d4c2d
Taylor Holberton <taylorcholberton@gmail.com>
2017-05-01 14:59:42 -0700

    cloning alloy (fixed other clone paths)

--- setup.sh
+++ setup.sh
@@ -6,20 +6,26 @@ set -u
 mkdir -p src
 mkdir -p dst
 
+if [ ! -e src/Alloy ]; then
+  git clone https://github.com/ReturnInfinity/Alloy.git src/Alloy
+else
+  git --git-dir=src/Alloy/.git pull origin master
+fi
+
 if [ ! -e src/BMFS ]; then
-  git clone https://github.com/ReturnInfinity/BMFS.git
+  git clone https://github.com/ReturnInfinity/BMFS.git src/BMFS
 else
   git --git-dir=src/BMFS/.git pull origin master
 fi
 
 if [ ! -e src/Pure64 ]; then
-  git clone https://github.com/ReturnInfinity/Pure64.git
+  git clone https://github.com/ReturnInfinity/Pure64.git src/Pure64
 else
   git --git-dir=src/Pure64/.git pull origin master
 fi
 
 if [ ! -e src/BareMetal-OS ]; then
-  git clone https://github.com/ReturnInfinity/BareMetal-OS.git
+  git clone https://github.com/ReturnInfinity/BareMetal-OS.git src/BareMetal-OS
 else
   git --git-dir=src/BareMetal-OS/.git pull origin master
 fi
