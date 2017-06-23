3eb1fb4037c236a563359e7653459e19bb193739
Ian Seyler <ian.seyler@returninfinity.com>
2017-06-23 14:42:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd bin
 echo Writing Pure64+Software
-cat pure64.sys kernel64.sys > software.sys
+cat pure64.sys kernel.sys > software.sys
 dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc
