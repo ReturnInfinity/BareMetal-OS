a9fcf055d48f880817507c9d80a79c3ac23a72ed
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-10 16:42:45 -0400

    added alloy to software.sys

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 cd bin
 echo Writing Pure64+Software
-cat pure64.sys kernel.sys > software.sys
+cat pure64.sys kernel.sys alloy.bin > software.sys
 dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc
