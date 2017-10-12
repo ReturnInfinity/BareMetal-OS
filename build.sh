b341d0e213f0a9dd1750dee55d5ac406a7399a26
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-11 22:13:45 -0400

    building baremetal libc

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-export topdir=$PWD
+set -e
+set -u
+
+export BMFS_INCLUDE_DIR=$PWD/src/BMFS/include
+export BMFS_LIBRARY=$PWD/src/BMFS/src/libbmfs.a
+
+export BAREMETAL_LIBC_INCLUDE_DIR=$PWD/src/BareMetal-libc/include
+export BAREMETAL_LIBC_LIBRARY=$PWD/bin/libbaremetal.a
 
 cd src
 
@@ -18,3 +25,9 @@ cd BareMetal-kernel
 ./build_x86-64.sh
 mv *.sys ../../bin
 mv *.txt ../../bin
+cd ..
+
+cd BareMetal-libc
+./build.sh
+mv libbaremetal.a ../../bin
+cd ..
