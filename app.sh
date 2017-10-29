5ce7770141091fd481676d87c6c5d505790773b2
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-29 15:00:02 -0400

    simplified app.sh a bit

--- app.sh
+++ app.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-cd src/Coreutils/
-nasm $1.asm -o ../../bin/$1.app
-if [ $? -eq 0 ]; then
-cd ../../bin
+
+set -u
+set -e
+
+nasm -Isrc/Coreutils/ src/Coreutils/$1.asm -o bin/$1.app
+cd bin
 ./bmfs bmfs.image create $1.app 2
 ./bmfs bmfs.image write $1.app
 cd ..
-else
-echo "Error"
-fi
