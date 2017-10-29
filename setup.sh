aa0e1800e3d8a65f87ad1650ef879c6a12fc3aca
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-29 15:25:10 -0400

    added project submodules

--- setup.sh
+++ setup.sh
@@ -3,23 +3,9 @@
 set -o errexit
 set -u
 
-mkdir -p src
 mkdir -p bin
 
-function clone {
-  if [ ! -e src/$1 ]; then
-    git clone https://github.com/ReturnInfinity/$1 src/$1
-  else
-    git --git-dir=src/$1/.git pull origin master
-  fi
-}
-
-clone Alloy
-clone BMFS
-clone Coreutils
-clone Pure64
-clone BareMetal-kernel
-clone BareMetal-libc
+git submodule update --init
 
 make -C src/BMFS NO_FUSE=1 NO_UTIX_UTILS=1 PREFIX=$PWD install
 
