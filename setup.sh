7b7fc44f2d7c38fc0ef74a7fe6847410e0a26ca5
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 20:20:57 -0500

    no longer using GNU make for BMFS

--- setup.sh
+++ setup.sh
@@ -7,10 +7,6 @@ mkdir -p bin
 
 git submodule update --init --recursive
 
-make -C src/BMFS NO_FUSE=1 NO_UTIX_UTILS=1 PREFIX=$PWD install
-
-bin/bmfs bin/bmfs.image initialize 128M
-
 ./build.sh
 ./format.sh
 ./install.sh
