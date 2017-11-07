67ec5a30a020b8af18f9a69faa07c59154c7d56a
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 20:20:15 -0500

    silencing dd, initializing disk here, stopping on error

--- format.sh
+++ format.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+set -e
+
 cd bin
 echo Formatting Disk Image
+./bmfs bmfs.image initialize 128M
 ./bmfs bmfs.image format /force
 echo Writing Master Boot Record
-dd if=bmfs_mbr.sys of=bmfs.image bs=512 conv=notrunc
+dd if=bmfs_mbr.sys of=bmfs.image bs=512 conv=notrunc 2>/dev/null
