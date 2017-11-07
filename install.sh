bcdd4c7dfb58ac8ff01383678882a70093eba986
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 20:20:42 -0500

    stopping on error, silencing dd

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -e
+
 cd bin
 echo Writing Pure64+Software
 cat pure64.sys kernel.sys alloy.bin > software.sys
-dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc
+dd if=software.sys of=bmfs.image bs=512 seek=16 conv=notrunc 2>/dev/null
