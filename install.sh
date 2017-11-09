75436266ae8513e2c97f828506f41a34d7643607
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-09 17:36:37 -0500

    updated kernel.sys to kernel.bin

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ export OUTPUT_DIR="$PWD/output"
 cd "$OUTPUT_DIR"
 echo Writing Pure64+Software
 cat "$OUTPUT_DIR/system/pure64.sys" \
-    "$OUTPUT_DIR/system/kernel.sys" \
+    "$OUTPUT_DIR/system/kernel.bin" \
     "$OUTPUT_DIR/system/alloy.bin" > "$OUTPUT_DIR/system/software.sys"
 dd if="$OUTPUT_DIR/system/software.sys" of="$OUTPUT_DIR/baremetal-os.img" bs=512 seek=16 conv=notrunc
