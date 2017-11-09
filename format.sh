b86b8f4cccc37af4c01113aa23315f85e7e43858
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-08 21:43:03 -0500

    creating a 'programs' directory, to put apps in

--- format.sh
+++ format.sh
@@ -6,5 +6,6 @@ cd bin
 echo Formatting Disk Image
 ./bmfs bmfs.image initialize 128M
 ./bmfs bmfs.image format /force
+./bmfs bmfs.image mkdir programs
 echo Writing Master Boot Record
 dd if=bmfs_mbr.sys of=bmfs.image bs=512 conv=notrunc 2>/dev/null
