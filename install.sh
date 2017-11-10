3027157773e76b888ce6237f4b6d91a70d3493eb
Ian Seyler <ian.seyler@returninfinity.com>
2017-11-10 15:01:09 -0500

    Also copy the boot sector on install

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ echo Writing Pure64+Software
 cat "$OUTPUT_DIR/system/pure64.sys" \
     "$OUTPUT_DIR/system/kernel.bin" \
     "$OUTPUT_DIR/system/alloy.bin" > "$OUTPUT_DIR/system/software.sys"
+dd if="$OUTPUT_DIR/system/bmfs_mbr.sys" of="$OUTPUT_DIR/baremetal-os.img" conv=notrunc
 dd if="$OUTPUT_DIR/system/software.sys" of="$OUTPUT_DIR/baremetal-os.img" bs=512 seek=16 conv=notrunc
