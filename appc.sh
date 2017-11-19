d3a2325337e674644eb5ea65a6d9f332239ec5b7
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-18 22:50:59 -0500

    using -fpie instead of -fPIC

--- appc.sh
+++ appc.sh
@@ -8,7 +8,7 @@ export OUTPUT_DIR="$PWD/output"
 CC=gcc
 CFLAGS=
 CFLAGS="${CFLAGS} -Wall -Wextra -Werror -Wfatal-errors -std=gnu99"
-CFLAGS="${CFLAGS} -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -fPIC"
+CFLAGS="${CFLAGS} -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -fpie"
 CFLAGS="${CFLAGS} -I$OUTPUT_DIR/include"
 CFLAGS="${CFLAGS} -Wl,-T src/Coreutils/coreutil.ld"
 LIBS="$OUTPUT_DIR/lib/libc.a $OUTPUT_DIR/lib/libbmfs.a"
