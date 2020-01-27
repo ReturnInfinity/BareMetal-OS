2b7e398b015e40018c618e301d21b3a6a08f7ff8
Ian Seyler <ian@seyler.me>
2020-01-27 14:16:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 export OUTPUT_DIR="$PWD/sys"
 
 cd "$OUTPUT_DIR"
-echo Building OS image.
+echo Building OS image...
 
 if [ "$1" != "" ]; then
 	cat pure64.sys kernel.sys $1 > software.sys
@@ -13,6 +13,6 @@ else
 	cat pure64.sys kernel.sys null.bin > software.sys
 fi
 
-dd if=mbr.sys of=disk.img conv=notrunc
-dd if=software.sys of=disk.img bs=512 seek=16 conv=notrunc
+dd if=mbr.sys of=disk.img conv=notrunc > /dev/null 2>&1
+dd if=software.sys of=disk.img bs=512 seek=16 conv=notrunc > /dev/null 2>&1
 cd ..
