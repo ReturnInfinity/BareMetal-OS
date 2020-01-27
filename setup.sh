644a8d94558e19e2505bd84da1a707c7dd534175
Ian Seyler <ian@seyler.me>
2020-01-27 14:03:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,11 @@ mkdir src
 mkdir sys
 cd src
 
-git clone https://github.com/ReturnInfinity/Pure64.git
-git clone https://github.com/ReturnInfinity/BareMetal.git
-git clone https://github.com/ReturnInfinity/BareMetal-Monitor.git
-git clone https://github.com/ReturnInfinity/BMFS.git
+echo Pulling code from GitHub...
+git clone https://github.com/ReturnInfinity/Pure64.git -q
+git clone https://github.com/ReturnInfinity/BareMetal.git -q
+git clone https://github.com/ReturnInfinity/BareMetal-Monitor.git -q
+git clone https://github.com/ReturnInfinity/BMFS.git -q
 
 cd ..
 
@@ -17,6 +18,7 @@ cd src/BareMetal-Monitor
 ./setup.sh
 cd ../..
 
+echo Creating disk image...
 cd sys
 dd if=/dev/zero of=disk.img count=128 bs=1048576
 dd if=/dev/zero of=null.bin count=8 bs=1
