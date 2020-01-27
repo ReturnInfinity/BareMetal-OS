e74da359c22b5d66e24cbad082ec4325697ed5d5
Ian Seyler <ian@seyler.me>
2020-01-27 14:15:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,11 +19,11 @@ dd if=/dev/zero of=disk.img count=128 bs=1048576 > /dev/null 2>&1
 dd if=/dev/zero of=null.bin count=8 bs=1 > /dev/null 2>&1
 cd ..
 
-./build.sh
-echo Building src/BareMetal-Monitor
 cd src/BareMetal-Monitor
 ./setup.sh
 cd ../..
+
+./build.sh
 
 cd sys
 ./bmfs disk.img format
