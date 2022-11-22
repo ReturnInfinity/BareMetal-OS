361ca4c6588916d534ce0c86c5480821929af4a4
Ian Seyler <ian@macbook-air.ht.home>
2022-11-22 21:10:14 +0000

    run script updates - Remove CPU name - Add NVMe config

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 
 cmd=( qemu-system-x86_64
 	-machine q35
-	-cpu core2duo
 # Window title in graphics mode
 	-name "BareMetal OS"
 # Boot a multiboot kernel file
@@ -19,6 +18,8 @@ cmd=( qemu-system-x86_64
 	-drive id=disk0,file="sys/disk.img",if=none,format=raw
 	-device ahci,id=ahci
 	-device ide-hd,drive=disk0,bus=ahci.0
+	-drive id=disk1,file="sys/disk1.img",if=none,format=raw
+	-device nvme,serial=OMG-NVME,drive=disk1
 # Ouput network to file
 #	-net dump,file=net.pcap
 # Output serial to file
