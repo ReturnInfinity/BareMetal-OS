fbf10eed0167d31b531f5f5f331d23bae436f682
Ian Seyler <ian@ians-macbook-air.local>
2022-12-13 19:50:23 +0000

    Script updates for UEFI

--- build.sh
+++ build.sh
@@ -21,22 +21,20 @@ function build_dir {
 	cd "$EXEC_DIR"
 }
 
-function update_file {
-	mv "$1" "$2"
-}
-
 build_dir "src/Pure64"
 build_dir "src/BareMetal"
 build_dir "src/BareMetal-Monitor"
 build_dir "src/BMFS"
 
-update_file "src/Pure64/bin/mbr.sys" "${OUTPUT_DIR}/mbr.sys"
-update_file "src/Pure64/bin/multiboot.sys" "${OUTPUT_DIR}/multiboot.sys"
-update_file "src/Pure64/bin/multiboot2.sys" "${OUTPUT_DIR}/multiboot2.sys"
-update_file "src/Pure64/bin/pure64.sys" "${OUTPUT_DIR}/pure64.sys"
-update_file "src/Pure64/bin/pxestart.sys" "${OUTPUT_DIR}/pxestart.sys"
-update_file "src/BareMetal/bin/kernel.sys" "${OUTPUT_DIR}/kernel.sys"
-update_file "src/BareMetal/bin/kernel-debug.txt" "${OUTPUT_DIR}/kernel-debug.txt"
-update_file "src/BareMetal-Monitor/bin/monitor.bin" "${OUTPUT_DIR}/monitor.bin"
-update_file "src/BareMetal-Monitor/bin/monitor-debug.txt" "${OUTPUT_DIR}/monitor-debug.txt"
-update_file "src/BMFS/bin/bmfs" "${OUTPUT_DIR}/bmfs"
+mv "src/Pure64/bin/mbr.sys" "${OUTPUT_DIR}/mbr.sys"
+mv "src/Pure64/bin/multiboot.sys" "${OUTPUT_DIR}/multiboot.sys"
+mv "src/Pure64/bin/multiboot2.sys" "${OUTPUT_DIR}/multiboot2.sys"
+mv "src/Pure64/bin/pure64.sys" "${OUTPUT_DIR}/pure64.sys"
+mv "src/Pure64/bin/pure64-debug.txt" "${OUTPUT_DIR}/pure64-debug.txt"
+mv "src/Pure64/bin/pxestart.sys" "${OUTPUT_DIR}/pxestart.sys"
+mv "src/Pure64/bin/uefi.sys" "${OUTPUT_DIR}/uefi.sys"
+mv "src/BareMetal/bin/kernel.sys" "${OUTPUT_DIR}/kernel.sys"
+mv "src/BareMetal/bin/kernel-debug.txt" "${OUTPUT_DIR}/kernel-debug.txt"
+mv "src/BareMetal-Monitor/bin/monitor.bin" "${OUTPUT_DIR}/monitor.bin"
+mv "src/BareMetal-Monitor/bin/monitor-debug.txt" "${OUTPUT_DIR}/monitor-debug.txt"
+mv "src/BMFS/bin/bmfs" "${OUTPUT_DIR}/bmfs"
