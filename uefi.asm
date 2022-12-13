f57ba3a57918ef1fc06f76208a051535fa9479ad
Ian Seyler <ian@ians-macbook-air.local>
2022-12-13 02:48:20 +0000

    UEFI boot works (sort of) - Correctly copies Pure64 to 0x8000 - Switches to 32-bit mode - Jumps to Pure64
    Pure64 then stops as it is looking for BIOS-style data

--- uefi.asm
+++ uefi.asm
@@ -112,23 +112,14 @@ EntryPoint:
 	mov [RTS], rax
 
 	; Find the interface to output services via its GUID
-	mov rcx, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_GUID		; *Protocol
-	mov rdx, 0						; *Registration OPTIONAL
-	mov r8, OUTPUT						; **Interface
+	mov rcx, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_GUID		; IN EFI_GUID *Protocol
+	mov rdx, 0						; IN VOID *Registration OPTIONAL
+	mov r8, OUTPUT						; OUT VOID **Interface
 	mov rax, [BS]
 	mov rax, [rax + EFI_BOOT_SERVICES_LOCATEPROTOCOL]
 	call rax
 	cmp rax, EFI_SUCCESS
 	jne failure
-
-	; Get the current ModeNumber
-
-	; Get current screen settings
-	mov rcx, [OUTPUT]					; IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
-	mov rdx, 0						; IN UINTN ModeNumber
-	mov r8, Columns						; OUT UINTN *Columns
-	mov r9, Rows						; OUT UINTN *Rows
-	call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_QUERY_MODE]
 
 	; Set screen colour attributes
 	mov rcx, [OUTPUT]					; IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
@@ -167,9 +158,9 @@ get_memmap:
 	; 40 UINT64 - Blank
 
 	; Find the interface to GRAPHICS_OUTPUT_PROTOCOL via its GUID
-	mov rcx, EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID		; *Protocol
-	mov rdx, 0						; *Registration OPTIONAL
-	mov r8, VIDEO						; **Interface
+	mov rcx, EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID		; IN EFI_GUID *Protocol
+	mov rdx, 0						; IN VOID *Registration OPTIONAL
+	mov r8, VIDEO						; OUT VOID **Interface
 	mov rax, [BS]
 	mov rax, [rax + EFI_BOOT_SERVICES_LOCATEPROTOCOL]
 	call rax
@@ -177,7 +168,6 @@ get_memmap:
 	jne failure
 
 	; Parse the graphics information
-	;
 	; Mode Structure
 	; 0  UINT32 - MaxMode
 	; 4  UINT32 - Mode
@@ -185,14 +175,6 @@ get_memmap:
 	; 16 UINTN - SizeOfInfo
 	; 24 EFI_PHYSICAL_ADDRESS - FrameBufferBase
 	; 32 UINTN - FrameBufferSize
-	;
-	; EFI_GRAPHICS_OUTPUT_MODE_INFORMATION Structure
-	; 0  UINT32 - Version
-	; 4  UINT32 - HorizontalResolution
-	; 8  UINT32 - VerticalResolution
-	; 12 EFI_GRAPHICS_PIXEL_FORMAT - PixelFormat (UINT32)
-	; 16 EFI_PIXEL_BITMASK - PixelInformation (4 UINT32 - RedMask, GreenMask, BlueMask, ReservedMask)
-	; 32 UINT32 - PixelsPerScanLine (Should be the same as HorizontalResolution)
 	mov rbx, [VIDEO]
 	add rbx, EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE
 	mov rbx, [rbx]						; RAX holds the address of the Mode structure
@@ -201,18 +183,18 @@ get_memmap:
 	mov rax, [rbx+32]					; RAX holds the FB size
 	mov [FBS], rax						; Save the FB size
 	mov rbx, [rbx+8]					; RBX holds the address of the EFI_GRAPHICS_OUTPUT_MODE_INFORMATION Structure
+	; EFI_GRAPHICS_OUTPUT_MODE_INFORMATION Structure
+	; 0  UINT32 - Version
+	; 4  UINT32 - HorizontalResolution
+	; 8  UINT32 - VerticalResolution
+	; 12 EFI_GRAPHICS_PIXEL_FORMAT - PixelFormat (UINT32)
+	; 16 EFI_PIXEL_BITMASK - PixelInformation (4 UINT32 - RedMask, GreenMask, BlueMask, ReservedMask)
+	; 32 UINT32 - PixelsPerScanLine (Should be the same as HorizontalResolution)
 	mov eax, [rbx+4]					; RAX holds the Horizontal Resolution
 	mov [HR], rax						; Save the Horizontal Resolution
 	mov eax, [rbx+8]					; RAX holds the Vertical Resolution
 	mov [VR], rax						; Save the Vertical Resolution
 	; TODO - Check EFI_GRAPHICS_PIXEL_FORMAT (RBX+12) to make sure bit 0 is set (32-bit colour mode), otherwise parse EFI_PIXEL_BITMASK  
-
-	; Clear the screen via the frame buffer
-;	mov rdi, [FB]
-;	mov eax, 0x00404040
-;	mov rcx, [FBS]
-;	shr rcx, 2						; Quick divide by 4 (32-bit colour)
-;	rep stosd
 
 	; Disable watchdog timer
 	xor ecx, ecx						; IN UINTN Timeout
@@ -225,8 +207,7 @@ get_memmap:
 	jne failure
 
 	; Copy Pure64 to the correct memory address
-	cli							; Stop interrupts
-	mov rsi, END
+	mov rsi, PAYLOAD
 	mov rdi, 0x8000
 	mov rcx, 16384						; Copy 16KB
 	rep movsb
@@ -234,7 +215,7 @@ get_memmap:
 	cmp ax, 0x3436						; Match against the Pure64 binary
 	jne sig_fail
 
-	; Exit Boot services
+	; Exit Boot services as EFI is no longer needed
 	mov rcx, [EFI_IMAGE_HANDLE]				; IN EFI_HANDLE ImageHandle
 	mov rdx, [memmapkey]					; IN UINTN MapKey
 	mov rbx, [BS]
@@ -242,17 +223,26 @@ get_memmap:
 	cmp rax, EFI_SUCCESS
 	jne failure
 
+	; Stop interrupts
+	cli
+
 	; Switch to 32-bit mode
+	lgdt [GDTR32]						; Load a 32-bit GDT
+	xor eax, eax						; Clear the segment registers
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov rax, cr0
+	btc rax, 31						; Clear PG (Bit 31)
+	mov cr0, rax
 
+BITS 32
 	; Call Pure64
-	mov rdi, [FB]
-	mov eax, 0x0000FF00					; Green
-	mov rcx, [FBS]
-	shr rcx, 2						; Quick divide by 4 (32-bit colour)
-	rep stosd
-	jmp $
+	jmp 8:0x8000						; 32-bit jump to set CS
 
-
+BITS 64
 failure:
 	mov rdi, [FB]
 	mov eax, 0x00FF0000					; Red
@@ -342,8 +332,6 @@ memmapsize:		dq 0
 memmapkey:		dq 0
 memmapdescsize:		dq 0
 memmapdescver:		dq 0
-Columns:		dq 0
-Rows:			dq 0
 
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_GUID:
 dd 0x387477c2
@@ -357,13 +345,28 @@ db 0x96,0xfb,0x7a,0xde,0xd0,0x80,0x51,0x6a
 
 hextable: 		db '0123456789ABCDEF'
 msg_start:		dw u('UEFI '), 0
-msg_failure:		dw u('System failure - '), 0
+msg_failure:		dw u('System failure'), 0
 msg_SigFail:		dw u('- Bad Sig!'), 0
 msg_space:		dw u(' '), 0
 msg_newline:		db 0x0D, 0x00, 0x0A, 0x00, 0x00, 0x00
 tchar:			db 0, 0, 0, 0, 0, 0, 0, 0
 
+align 16
+GDTR32:					; Global Descriptors Table Register
+dw gdt32_end - gdt32 - 1		; limit of GDT (size minus one)
+dq gdt32				; linear address of GDT
+
+align 16
+gdt32:
+dw 0x0000, 0x0000, 0x0000, 0x0000	; Null descriptor
+dw 0xFFFF, 0x0000, 0x9A00, 0x00CF	; 32-bit code descriptor
+dw 0xFFFF, 0x0000, 0x9200, 0x00CF	; 32-bit data descriptor
+gdt32_end:
+
 align 4096
+PAYLOAD:
+
+align 65536				; Pad out to 64K
 DATA_END:
 END:
 
