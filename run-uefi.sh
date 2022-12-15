467a6fdd242c01f6d3dc4625a2aebbf91df5fba8
Ian Seyler <ian@ians-macbook-air.local>
2022-12-15 19:58:25 +0000

    Adjust UEFI to boot via a disk by default

--- run-uefi.sh
+++ run-uefi.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # from http://unix.stackexchange.com/questions/9804/how-to-comment-multi-line-commands-in-shell-scripts
 
-mformat -t 128 -h 2 -n 1024 -C -F -i sys/fat.img
-mmd -i sys/fat.img ::/EFI
-mmd -i sys/fat.img ::/EFI/BOOT
-mcopy -oi fat.img sys/BOOTX64.EFI ::/EFI/BOOT/BOOTX64.EFI
+cd sys
+mformat -t 128 -h 2 -n 1024 -C -F -i fat.img
+mmd -i fat.img ::/EFI
+mmd -i fat.img ::/EFI/BOOT
+mcopy -oi fat.img BOOTX64.EFI ::/EFI/BOOT/BOOTX64.EFI
+cd ..
 
 cmd=( qemu-system-x86_64
 	-machine q35
@@ -18,11 +20,11 @@ cmd=( qemu-system-x86_64
 #       -netdev socket,id=testnet,connect=127.0.0.1:1234
 # Disk configuration
 # File system folder
-	-drive format=raw,file=fat:rw:sys/drive
+#	-drive format=raw,file=fat:rw:sys/drive
 # AHCI
-#	-drive id=disk0,file="sys/fat.img",if=none,format=raw
-#	-device ahci,id=ahci
-#	-device ide-hd,drive=disk0,bus=ahci.0
+	-drive id=disk0,file="sys/fat.img",if=none,format=raw
+	-device ahci,id=ahci
+	-device ide-hd,drive=disk0,bus=ahci.0
 # NVMe
 #	-drive id=disk1,file="sys/disk1.img",if=none,format=raw
 #	-device nvme,drive=disk1
