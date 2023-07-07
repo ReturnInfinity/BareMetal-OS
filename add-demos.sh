32328c15bcd965536163161744c1f85b9fad45bc
Ian Seyler <ian@ians-macbook-air.local>
2023-07-06 20:18:03 -0400

    Replace counter.app with euler1.app

--- add-demos.sh
+++ add-demos.sh
@@ -9,8 +9,8 @@ cd ../../../sys/
 ./bmfs disk.img write ethtest.app
 ./bmfs disk.img create sysinfo.app 2
 ./bmfs disk.img write sysinfo.app
-./bmfs disk.img create counter.app 2
-./bmfs disk.img write counter.app
+./bmfs disk.img create euler1.app 2
+./bmfs disk.img write euler1.app
 ./bmfs disk.img create helloc.app 2
 ./bmfs disk.img write helloc.app
 ./bmfs disk.img create gavare.app 2
